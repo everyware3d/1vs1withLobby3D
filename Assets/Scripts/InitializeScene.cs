365c8fed61e7dcaf21bfc4c1850c263b565957f7
blainebell <blaine@blainebell.org>
2025-11-01 22:11:59 -0700

    shift around files

--- Assets/Scripts/InitializeScene.cs
+++ Assets/Scripts/InitializeScene.cs
@@ -3,9 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class InitializeLobbyScene : MonoBehaviour
 {
+    // public Scene sceneToLoad;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+        // SceneManager.LoadScene(sceneToLoad.name);
         SceneManager.LoadScene("Lobby");
     }
 }
